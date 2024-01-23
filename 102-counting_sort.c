327aa5ca93c2135e362ec70313eac85d7e5db39b
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-01-23 17:50:40 +0100

    Update 102-counting_sort.c

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,49 +1,49 @@
 #include "sort.h"
 
 /**
- * counting_sort - Sorts an array of integers in ascending order using
- *                 the Counting Sort algorithm
- * @array: The array to be sorted
- * @size: Number of elements in the array
- */
+* counting_sort - counting sort algorithm
+* @array: array to be sorted
+* @size: size of the array to be sorted
+*/
 void counting_sort(int *array, size_t size)
 {
-    if (array == NULL || size < 2)
-        return;
+	int i, max;
+	int *count = NULL, *copy = NULL;
+	size_t j, temp, total = 0;
 
-    /* Find the maximum value in the array */
-    int max = array[0];
-    for (size_t i = 1; i < size; i++)
-    {
-        if (array[i] > max)
-            max = array[i];
-    }
-
-    /* Create and initialize the counting array */
-    int *counting_array = malloc((max + 1) * sizeof(int));
-    if (counting_array == NULL)
-        return;
-
-    for (int i = 0; i <= max; i++)
-        counting_array[i] = 0;
-
-    /* Populate the counting array with the frequency of each element */
-    for (size_t i = 0; i < size; i++)
-        counting_array[array[i]]++;
-
-    /* Print the counting array */
-    print_array(counting_array, max + 1);
-
-    /* Update the original array with sorted values */
-    for (int i = 0, j = 0; i <= max; i++)
-    {
-        while (counting_array[i] > 0)
-        {
-            array[j++] = i;
-            counting_array[i]--;
-        }
-    }
-
-    /* Free the dynamically allocated counting array */
-    free(counting_array);
+	if (array == NULL || size < 2)
+		return;
+	copy = malloc(sizeof(int) * size);
+	if (copy == NULL)
+		return;
+	for (j = 0, max = 0; j < size; j++)
+	{
+		copy[j] = array[j];
+		if (array[j] > max)
+			max = array[j];
+	}
+	count = malloc(sizeof(int) * (max + 1));
+	if (count == NULL)
+	{
+		free(copy);
+		return;
+	}
+	for (i = 0; i <= max; i++)
+		count[i] = 0;
+	for (j = 0; j < size; j++)
+		count[array[j]] += 1;
+	for (i = 0; i <= max; i++)
+	{
+		temp = count[i];
+		count[i] = total;
+		total += temp;
+	}
+	for (j = 0; j < size; j++)
+	{
+		array[count[copy[j]]] = copy[j];
+		count[copy[j]] += 1;
+	}
+	print_array(count, max + 1);
+	free(count);
+	free(copy);
 }
