089453e9d95f4c5113a3fc4b43f94f66052fb267
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-01-23 17:40:36 +0100

    Update 101-cocktail_sort_list.c

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,63 +1,62 @@
 #include "sort.h"
 
-void swap_nodes(listint_t *left, listint_t *right, listint_t **head);
+void move_left(listint_t *curr, listint_t *prev, listint_t **head);
 
 /**
- * cocktail_sort_list - Sorts a doubly linked list of integers in ascending order
- *                      using the Cocktail Shaker Sort algorithm
- * @list: Pointer to the head of the doubly linked list
- */
+* cocktail_sort_list - coctail sort in doubly linked list
+* @list: A Doubly linked list
+*/
+
 void cocktail_sort_list(listint_t **list)
 {
-    listint_t *current, *max, *min;
+	listint_t *cur;
+	listint_t *max = NULL;
+	listint_t *min = NULL;
 
-    if (!list || !(*list) || (*list)->next == NULL)
-        return;
-
-    current = *list;
-    do {
-        while (current->next)
-        {
-            if (current->n > current->next->n)
-                swap_nodes(current, current->next, list);
-            else
-                current = current->next;
-        }
-        max = current;
-
-        while (current->prev != min)
-        {
-            if (current->n < current->prev->n)
-                swap_nodes(current->prev, current, list);
-            else
-                current = current->prev;
-        }
-        min = current;
-    } while (min != max);
+	if (!list || !(*list) || (*list)->next == NULL)
+		return;
+	cur = *list;
+	do {
+		while (cur->next)
+		{
+			if (cur->n > cur->next->n)
+				move_left(cur->next, cur, list);
+			else
+				cur = cur->next;
+		}
+		max = cur;
+		while (cur->prev != min)
+		{
+			if (cur->n < cur->prev->n)
+				move_left(cur, cur->prev, list);
+			else
+				cur = cur->prev;
+		}
+		min = cur;
+	} while (min != max);
 }
 
 /**
- * swap_nodes - Swaps two nodes in a doubly linked list
- * @left: Left node to be swapped
- * @right: Right node to be swapped
- * @head: Pointer to the head of the doubly linked list
- */
-void swap_nodes(listint_t *left, listint_t *right, listint_t **head)
+* move_left - swaps two members of a list
+*
+* @curr: current node
+* @prev: previous node
+* @head: head of list
+*/
+void move_left(listint_t *curr, listint_t *prev, listint_t **head)
 {
-    listint_t *swap1 = right->next;
-    listint_t *swap2 = left->prev;
+	listint_t *swap1 = curr->next;
+	listint_t *swap2 = prev->prev;
 
-    if (swap1)
-        swap1->prev = left;
-    if (swap2)
-        swap2->next = right;
-
-    left->next = swap1;
-    right->prev = swap2;
-    left->prev = right;
-    right->next = left;
-
-    if (*head == left)
-        *head = right;
-    print_list(*head);
+	if (swap1 != NULL)
+		swap1->prev = prev;
+	if (swap2 != NULL)
+		swap2->next = curr;
+	curr->prev = swap2;
+	prev->next = swap1;
+	curr->next = prev;
+	prev->prev = curr;
+	if (*head == prev)
+		*head = curr;
+	print_list(*head);
 }
